ca704bcc43e16064258e306417281aeae9a75b7a
Soós Gábor <soos.gabor@jedlik.eu>
2023-12-21 22:30:34 +0100

    qwe

--- src/components/UserListItem.vue
+++ src/components/UserListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img
-      :src="props.user.image ? '/image/' + props.user.image : '/image/profile.png'"
+      :src="props.user.image ? '/testPage/image/' + props.user.image : '/testPage/image/profile.png'"
       alt="profile"
       width="30"
       class="rounded-circle shadow-sm"
