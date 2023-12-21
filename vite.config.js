d8f1d1448a2409523e0ca3a3788bb735bbf9ff59
Soós Gábor <soos.gabor@jedlik.eu>
2023-12-21 22:11:04 +0100

    vite.config.js change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://hoaxify-backend.onrender.com/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  },
   plugins: [
     vue(),
   ],
